c813d720333c2b0d04332c09260a7494bacaa8fd
Henrik Fricke <henrik.fricke@jimdo.com>
2016-06-20 13:29:36 +0200

    do not hook a new comment when comment by zendesk

--- app/githubzen/hooks.rb
+++ app/githubzen/hooks.rb
@@ -1,14 +1,17 @@
 class Hooks < Base
   post '/hooks' do
     return halt 406, 'Only "issue_comment" supported yet' unless params['action'] == "created"
-    body = erb :ticket, locals: {
-      text: params['comment']['body'],
-      developer: settings.octokit.user(params['comment']['user']['login']),
-      issue: params['issue']
-    }
-    ticket = settings.zendesk.search(query: "type:ticket external_id:#{params['issue']['url']}").first
-    ticket.comment = { value: body, public: false }
-    ticket.save!
+    user = settings.octokit.user(params['comment']['user']['login'])
+    unless user['email'] == ENV['ZENDESK_USERNAME'] then
+      body = erb :ticket, locals: {
+        text: params['comment']['body'],
+        developer: user,
+        issue: params['issue']
+      }
+      ticket = settings.zendesk.search(query: "type:ticket external_id:#{params['issue']['url']}").first
+      ticket.comment = { value: body, public: false }
+      ticket.save!
+    end
     [200, ['']]
   end
 end
